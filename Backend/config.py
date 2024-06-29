0c88d0371ba330305885cedcd084b732016b8503
M Ibrahim Faisal <hyibrahimfaisal@gmail.com>
2024-06-30 03:16:36 +0500

    added front-end to backend interactions

--- Backend/config.py
+++ Backend/config.py
@@ -1,21 +1,17 @@
-from flask_cors import CORS
-from flask import Flask, redirect
-from text_gen import get_response
-
+from flask import Flask, request, jsonify
+from flask_cors import CORS 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app=app)
 
+@app.route('/create_ppt', methods=['POST'])
+def create_ppt():
+        # Attempt to get the JSON data from the request
+        data = request.get_json()
+        print(data)
 
-#ROUTES
-@app.route("/create_ppt")
-def index():
-  get_response("Make a presentation in robots")
-  print(get_response)
-  return "Hello Py"
+        
+        return jsonify({'error': 'Internal server error'}), 500  # 500 Internal Server Error
 
-
-
-
-if __name__ == "__main__":
-  app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [inputData, setInputData] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //
+
+    fetch("http://127.0.0.1:5000/create_ppt", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(inputData),
+    });
+    // .then((response) => console.log(response.json())) // Handle response from server (optional)
+    // .catch((error) => console.error(error));
+  };
+
   return (
     <>
-      <form action="http://127.0.0.1:5000/create_ppt">
+      <form onSubmit={handleSubmit}>
         <label>Input Data</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={inputData}
+          onChange={(e) => setInputData(e.target.value)}
+        />
         <br />
         <button>Generate</button>
       </form>
