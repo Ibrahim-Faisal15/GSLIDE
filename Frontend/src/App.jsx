0bf874fec26dc04d05fd764c5e5d2bc484216095
M Ibrahim Faisal <hyibrahimfaisal@gmail.com>
2024-07-04 03:57:44 +0500

    Completed my project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Spinner from "react-bootstrap/Spinner";
 import "./index.css";
-import slider from "../public/slides.svg";
 
 const App = () => {
   const [inputData, setInputData] = useState("");
+  const [loadState, setloadState] = useState(false);
+  const [downloadReady, setdownloadReady] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     //
 
+    setloadState(true);
     fetch("http://127.0.0.1:5000/create_ppt", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -23,12 +27,16 @@ const App = () => {
         return blob;
       })
       .then((blob) => {
+        setloadState(false);
         const URL = window.URL.createObjectURL(blob);
         const link = document.createElement("a");
+
         link.href = URL;
         link.download = "file.pptx"; // Set default filename if not provided
         link.textContent = "Download File";
         document.body.appendChild(link);
+        link.classList.add("hidden");
+        link.click();
       })
       .catch((error) => {
         console.error("Error fetching or processing file2114:", error);
@@ -73,10 +81,16 @@ const App = () => {
           onChange={(e) => setInputData(e.target.value)}
           className="w-[30vw]  h-12 rounded-lg pl-3 text-xl font-semibold font-mono border-4 border-[#1c1b1b] focus:outline-none"
         />
-
-        <button className="  mt-4 h-12 w-44 text-xl  text-white font-sans  bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-blue-700 via-blue-800 to-gray-900 hover:bg-gradient-to-br  dark:focus:ring-cyan-800 font-medium rounded-lg  px-5 py-2.5 text-center me-2 mb-44">
-          Generate
-        </button>
+        {loadState ? (
+          <Spinner
+            animation="border"
+            className="mb-52 relative top-14  h-24 w-24 text-white "
+          />
+        ) : (
+          <button className="  mt-4 h-12 w-44 text-xl  text-white font-sans  bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-blue-700 via-blue-800 to-gray-900 hover:bg-gradient-to-br  dark:focus:ring-cyan-800 font-medium rounded-lg  px-5 py-2.5 text-center me-2 mb-44">
+            Generate
+          </button>
+        )}
       </form>
     </div>
   );
